c56b75b747854ca139adfee4d9cb80f01268a556
Neil Sabde <neilsabde@gmail.com>
2021-12-25 14:40:32 +0530

    Fifth Commit

--- App.js
+++ App.js
@@ -1,2 +1,20 @@
+import * as React from "react";
 import Main from "./components/Main";
-export default Main;
+import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
