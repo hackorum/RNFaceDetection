1b0d0e65bcae037cd370d3911406b9e4f1a33895
Neil Sabde <neilsabde@gmail.com>
2021-12-25 10:54:56 +0530

    Fourth Commit

---
+++ components/Filter.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+export default ({
+  face: {
+    bounds: {
+      size: { width: faceWidth, height: faceHeight },
+    },
+    leftEyePosition,
+    rightEyePosition,
+    noseBasePosition,
+  },
+  source,
+  width,
+  height,
+  left,
+  right,
+  top,
+}) => {
+  const filterWidth = faceWidth * width;
+  const filterHeight = faceHeight * height;
+
+  const transformAngle = (
+    angleRad = Math.atan(
+      (rightEyePosition.y - leftEyePosition.y) /
+        (rightEyePosition.x - leftEyePosition.x)
+    )
+  ) => (angleRad * 180) / Math.PI;
+
+  return (
+    <View
+      style={{
+        position: "absolute",
+        left: leftEyePosition.x - filterWidth * left,
+        right: rightEyePosition.x - filterWidth * right,
+        top: noseBasePosition.y - filterHeight * top,
+      }}
+    >
+      <Image
+        source={source}
+        style={{
+          width: filterWidth,
+          height: filterHeight,
+          resizeMode: "contain",
+          transform: [{ rotate: `${transformAngle()}deg` }],
+        }}
+      />
+    </View>
+  );
+};
